3483881dce3f8d0f91e174a69476a6d5da6211ac
Tiago Fernandez <tiago.fernandez@gmail.com>
2013-11-12 20:50:39 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Harena::Application.config.secret_key_base = '59111f622b09d0df57bc9dc0d2a32b4238ae72e94cc88485151f99395c94b04a95ffb596df87ceb2d8fa11bd8d9a194f0c7d40faeea81504ce8556ff0ad957a4'
