26064ac6c0ce0dbe8f76cb9e74266abde02b5b31
Tiago Fernandez <tiago.fernandez@gmail.com>
2013-11-13 21:27:20 +0100

    Validate username format: only letters and numbers, no spaces

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,9 +1,12 @@
 class Member < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
-  validates :username, :uniqueness => { :case_sensitive => false }, presence: true
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: /\A[a-zA-Z0-9]+\z/i, message: "should only contain letters and numbers" }
 
 end
