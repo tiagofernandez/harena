8ecb12e6a426e77e0ff74ae02b572d88e9392f47
Tiago Fernandez <tiago.fernandez@gmail.com>
2013-11-17 13:55:24 +0100

    Start at index 0

--- test/models/map_type_test.rb
+++ test/models/map_type_test.rb
@@ -7,13 +7,13 @@ class MapTypeTest < ActiveSupport::TestCase
   end
 
   test "should get code for each map" do
-    assert 1, MapType.get_key('GameBoard-hd')
-    assert 2, MapType.get_key('GameBoard02-hd')
-    assert 3, MapType.get_key('GameBoard03-hd')
-    assert 4, MapType.get_key('DwarfGameBoard-hd')
-    assert 5, MapType.get_key('OrcGameBoard-hd')
-    assert 6, MapType.get_key('GameBoard_TF2-hd')
-    assert 7, MapType.get_key('GameBoard_Shaolin-hd')
+    assert_equal 0, MapType.get_key('GameBoard-hd')
+    assert_equal 1, MapType.get_key('GameBoard02-hd')
+    assert_equal 2, MapType.get_key('GameBoard03-hd')
+    assert_equal 3, MapType.get_key('DwarfGameBoard-hd')
+    assert_equal 4, MapType.get_key('OrcGameBoard-hd')
+    assert_equal 5, MapType.get_key('GameBoard_TF2-hd')
+    assert_equal 6, MapType.get_key('GameBoard_Shaolin-hd')
     assert_raises(ArgumentError) { MapType.get_key('ProtossGameBoard-hd') }
   end
 
