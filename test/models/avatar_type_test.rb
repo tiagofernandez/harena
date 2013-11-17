626c6dc4c5b1c432b1c5576b68ae281a84d1f24d
Tiago Fernandez <tiago.fernandez@gmail.com>
2013-11-17 13:57:00 +0100

    Add avatar and emblem types

---
+++ test/models/avatar_type_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class AvatarTypeTest < ActiveSupport::TestCase
+
+  test "should list all avatars" do
+    assert_equal 122, AvatarType.all.size
+  end
+
+  test "should get code for each avatar" do
+    assert_equal 100, AvatarType.get_key('Annihilator_v1-hd.jpg')
+    assert_equal 215, AvatarType.get_key('Wraith_v2-hd.jpg')
+    assert_raises(ArgumentError) { AvatarType.get_key('About_HA_Logo-hd.png') }
+  end
+
+end
