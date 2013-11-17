98586be2be6d16209c53df73e8eed22ae029118e
Tiago Fernandez <tiago.fernandez@gmail.com>
2013-11-17 17:11:23 +0100

    Add utility method for generating select box options

--- app/models/concerns/enumerable.rb
+++ app/models/concerns/enumerable.rb
@@ -2,11 +2,16 @@ module Enumerable
   extend ActiveSupport::Concern
 
   module ClassMethods
+
     def get_key(value)
       self.all.each_pair do |key, val|
         return key if val == value
       end
       raise ArgumentError, "Value not found: #{value}"
+    end
+
+    def options
+      self.all.invert.map { |key, value| [key, value] }
     end
   end
 end
