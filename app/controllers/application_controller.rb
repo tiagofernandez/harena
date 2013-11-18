e1c04323e1913e16adcc88916c791fb033b36423
Tiago Fernandez <tiago.fernandez@gmail.com>
2013-11-18 21:54:54 +0100

    Fix avatar change on profile update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:username, :avatar]
-    devise_parameter_sanitizer.for(:sign_in) << [:username, :avatar]
+    devise_parameter_sanitizer.for(:sign_up)        << [:username, :avatar]
+    devise_parameter_sanitizer.for(:account_update) << [:username, :avatar]
   end
 end
