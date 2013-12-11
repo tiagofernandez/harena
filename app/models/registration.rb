fe9586706861ea1bf579d640603cc1c516697b36
Tiago Fernandez <tiago.fernandez@gmail.com>
2013-12-11 23:23:39 +0100

    Get registered players if the tournament hasn’t started yet

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,5 +1,5 @@
 class Registration < ActiveRecord::Base
   belongs_to :tournament
   belongs_to :player
-  # validates_presence_of :tournament, :player
+  validates_presence_of :tournament, :player
 end
